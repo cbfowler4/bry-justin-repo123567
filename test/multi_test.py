1a02c8d5727e6ee426715560d28c0c1277afdffb
Bryan <cbfowler4@gmail.com>
2017-03-24 00:25:37 +0000

    multiprocessing test case for jdes42

--- test/multi_test.py
+++ test/multi_test.py
@@ -1,22 +1,39 @@
 import multiprocessing, time
 
-def calc_square(numbers, q):
-    for n in numbers:
-        q.put(n*n)
-        time.sleep(.2)
+#def calc_square(numbers, q):
+#    for n in numbers:
+#        q.put(n*n)
+#        time.sleep(.2)
 
-def calc_double(q):
-    a = 2
-    while a>1:
-        num = q.get()
-        print(num*2)
+def test1():
+    a = 1
+    for n in xrange(4):
+        a = a + 1
+        print a
+        time.sleep(1)
+        
+#def calc_double(q):
+#    a = 2
+#    while a>1:
+#        num = q.get()
+#        print(num*2)
 
+def test2():
+    a = 1
+    for n in xrange(4):
+        a = a + 2
+        print a
+        time.sleep(1)
+    
 if __name__ == "__main__":
-    numbers = [2, 3, 5]
-    q = multiprocessing.Queue()
-    p = multiprocessing.Process(target=calc_square, args=(numbers, q))
-    p2 = multiprocessing.Process(target=calc_double, args=(q,))
+    #numbers = [2, 3, 5]
+    #q = multiprocessing.Queue()
+    #p = multiprocessing.Process(target=calc_square, args=(numbers, q))
+    #p2 = multiprocessing.Process(target=calc_double, args=(q,))
 
+    p = multiprocessing.Process(target = test1)
+    p2 = multiprocessing.Process(target = test2)
+    
     p.start()
     p2.start()
 
